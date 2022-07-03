912dc4dc091bf8df09c0d26f2f5640386115f524
Vasilii <bogdanovva84@mail.ru>
2022-07-04 00:03:16 +0300

    Добавлена задача 10

---
+++ DZ_Z10/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное
+// число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+namespace HomeWork
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Введите трёхзначное число: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number > 999 || number < 100) 
+            {
+                Console.WriteLine("We cannot");
+            } 
+            else 
+            {
+                Console.WriteLine((number / 10) % 10);
+            }
+            
+        
+        }
+    }
+}
