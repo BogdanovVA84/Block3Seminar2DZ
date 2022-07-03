4a08815e6f2b0c3f8e699e75064927bd87a1b7bb
Vasilii <bogdanovva84@mail.ru>
2022-07-04 00:29:54 +0300

    Добавлена задача 15

---
+++ DZ_Z15/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+
+namespace HomeWork
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number > 7 || number < 1) 
+            {
+                Console.WriteLine("Это не день недели");
+            } 
+            else if (number < 6) 
+            {
+                Console.WriteLine("Нет");
+            } 
+            else 
+            {
+                Console.WriteLine("Да");
+            }
+        
+        }
+    }
+}
